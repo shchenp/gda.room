4ff1a3eac96d7fdb84a8ae80e023507fe2731ecb
Nikita <np.shche@gmail.com>
2023-11-28 13:36:41 +0300

    remove GlobalConstants.cs

--- Assets/Scripts/GlobalConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace DefaultNamespace
-{
-    public class GlobalConstants
-    {
-        public const string DOOR_TAG = "Door";
-        public const string INTERACTABLE_ITEM_TAG = "Interactable";
-    }
-}
